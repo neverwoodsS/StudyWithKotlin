2322d11cf4962ad08f53fb938764780667568e99
张璐琳 <413700858@qq.com>
2017-02-14 17:49:02 +0800

    subway 算法添加注释

--- src/subway/solution1/Station.kt
+++ src/subway/solution1/Station.kt
@@ -6,6 +6,12 @@ package subway.solution1
 class Station(val name: String) {
     val throughLines = mutableListOf<TrainLine>()
 
+    /**
+     * 计算两个站点之间的距离
+     * station: 目标站
+     * lines: 已经走过的路线
+     * deep: 递归深度(换乘次数)
+     */
     fun farFrom(station: Station, lines: MutableSet<TrainLine> = mutableSetOf(), deep: Int = 0): Int {
         // 限制换乘次数
         if (deep > 2) {
@@ -13,6 +19,7 @@ class Station(val name: String) {
         }
 
         if (isDirectArrive(station)) {
+            // 在同一条线路上则直接计算距离
             var min = 25
             throughLines
                     .filter { it.stations.contains(station) }
@@ -30,21 +37,34 @@ class Station(val name: String) {
             var tempLines = mutableSetOf<TrainLine>()
             lines.forEach { tempLines.add(it) }
 
+            // 遍历经过当前站点的线路
             for (throughLine in throughLines) {
+
+                // 排除掉已经走过的线路，避免无限换乘
                 if (!tempLines.contains(throughLine)) {
+
+                    // 遍历线路上的所有换乘点
                     for (changeStation in throughLine.changeStations) {
+
+                        // 排除掉通过自身换乘的情景
                         if (changeStation != this) {
+
                             val newLines = mutableSetOf<TrainLine>()
                             tempLines.forEach { newLines.add(it) }
                             newLines.add(throughLine)
 
+                            // 计算当前站点到所选换乘点需要经过的距离
                             val changeDistance = throughLine.countDistance(this, changeStation)
+
+                            // 计算换乘点到目标点的距离
+                            // 通过调用方法自身实现递归遍历
                             val deepDistance = changeStation.farFrom(station, newLines, deep + 1)
 
                             if (deepDistance >= Int.MAX_VALUE) {
                                 continue
                             }
 
+                            // 发现了更近的距离则保存
                             val distance = changeDistance + deepDistance
                             if (distance < min) {
                                 min = distance
@@ -60,12 +80,19 @@ class Station(val name: String) {
         }
     }
 
+    /**
+     * 获取两个站点之间的最短路径 的集合
+     * station: 目标站
+     * lines: 已经走过的路线
+     * deep: 递归深度(换乘次数)
+     */
     fun pathTo(station: Station, lines: MutableSet<TrainLine> = mutableSetOf(), deep: Int = 0): MutableList<MutableList<Pair<Station, TrainLine>>?>? {
         // 限制换乘次数
         if (deep > 2) {
             return null
         }
 
+        // 用于保存所有距离最近的路线
         val mins = mutableListOf<MutableList<Pair<Station, TrainLine>>?>()
         var minSize = 25
 
@@ -77,10 +104,13 @@ class Station(val name: String) {
                     .forEach {
                         temp = it.getPathBetween(this@Station, station)
 
+                        // 如果距离等于当前最小距离，共存
                         if (temp.size == minSize) {
                             mins.add(temp)
+                        // 如果距离小于当前最小距离，清除先前数据再保存
                         } else if (temp.size < minSize) {
                             minSize = temp.size
+                            mins.clear()
                             mins.add(temp)
                         }
                     }
@@ -90,35 +120,45 @@ class Station(val name: String) {
             var tempLines = mutableSetOf<TrainLine>()
             lines.forEach { tempLines.add(it) }
 
+            // 遍历经过当前站点的线路
             for (throughLine in throughLines) {
+
+                // 排除掉已经走过的线路，避免无限换乘
                 if (!tempLines.contains(throughLine)) {
+
+                    // 遍历线路上的所有换乘点
                     for (changeStation in throughLine.changeStations) {
+
+                        // 排除掉通过自身换乘的情景
                         if (changeStation != this) {
 
                             val newLines = mutableSetOf<TrainLine>()
                             tempLines.forEach { newLines.add(it) }
                             newLines.add(throughLine)
 
+                            // 获取当前站到换乘站的路径
                             val changeList = throughLine.getPathBetween(this, changeStation)
+
+                            // 获取换乘站到目标站点的最短路径集合
+                            // 递归实现
                             val deepList = changeStation.pathTo(station, newLines, deep + 1)
 
+                            // 排除异常情况
                             if (deepList == null || deepList.isEmpty()) {
                                 continue
                             }
 
+                            // 计算总距离
                             val size = changeList.size + deepList[0]!!.size
 
-                            if (size == minSize) {
-                                deepList.forEach {
-                                    val list = mutableListOf<Pair<Station, TrainLine>>()
-                                    list.addAll(changeList)
-                                    list.addAll(it!!)
-                                    mins.add(list)
+                            // 如果距离等于当前最小距离，共存
+                            // 如果距离小于当前最小距离，清除先前数据再保存
+                            if (size <= minSize) {
+                                if (size < minSize) {
+                                    // 先清除
+                                    mins.clear()
+                                    minSize = size
                                 }
-                            } else if (size < minSize) {
-                                // 先清除
-                                mins.clear()
-                                minSize = size
 
                                 deepList.forEach {
                                     val list = mutableListOf<Pair<Station, TrainLine>>()
@@ -138,6 +178,9 @@ class Station(val name: String) {
         }
     }
 
+    /**
+     * 判断某站和当前站点是否在同一条线路上
+     */
     fun isDirectArrive(station: Station): Boolean {
         throughLines
                 .filter { it.stations.contains(station) }
--- src/subway/solution1/TrainLine.kt
+++ src/subway/solution1/TrainLine.kt
@@ -4,9 +4,15 @@ package subway.solution1
  * Created by zhangll on 2017/2/8.
  */
 class TrainLine(val name: String) {
+    // 线路经过的所有站点集合
     val stations = linkedSetOf<Station>()
+
+    // 线路经过的所有 换乘点 集合
     val changeStations = linkedSetOf<Station>()
 
+    /* 将站点添加至线路中
+     * stationName: 站名
+     */
     fun putStation(stationName: String) {
         val station = StationPool.getStation(stationName)
         // station 与 trainLine 互相添加引用
@@ -21,13 +27,19 @@ class TrainLine(val name: String) {
         }
     }
 
+    /**
+     * 添加换乘站
+     */
     fun putChangeStation(station: Station) {
         changeStations.add(station)
     }
 
+    /**
+     * 计算线路上两个站之间的距离
+     */
     fun countDistance(station1: Station, station2: Station): Int {
-        var index1: Int = -1
-        var index2: Int = -1
+        var index1: Int? = null
+        var index2: Int? = null
 
         stations.forEachIndexed { index, station ->
             if (station === station1) {
@@ -39,13 +51,19 @@ class TrainLine(val name: String) {
             }
         }
 
-        if (index1 == -1 || index2 == -1) {
+        if (index1 == null || index2 == null) {
             throw Exception("下标计算异常")
         }
 
-        return Math.abs(index1 - index2)
+        return Math.abs(index1!! - index2!!)
     }
 
+    /**
+     * 获取线路上两个站点之间的路径
+     * station1: 出发点
+     * station2: 目的点
+     * return: station-line 的 键值对 的集合，不直接用 station 集合是因为需要有 line 数据显示
+     */
     fun getPathBetween(station1: Station, station2: Station): MutableList<Pair<Station, TrainLine>> {
         val result = mutableListOf<Pair<Station, TrainLine>>()
         val index1 = stations.indexOf(station1)
@@ -59,7 +77,7 @@ class TrainLine(val name: String) {
             (index1 .. index2).forEach {
                 result.add(stations.elementAt(it) to this)
             }
-        } else {
+        } else { // 理论上不会出现
             stations.elementAt(index1)
         }
 
